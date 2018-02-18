8bf40929d6ead0936dc8d24783ae20a21644ba94
James Kamana <jaykamana@gmail.com>
2018-02-18 17:47:52 +0300

    Add FRD and Employees Model

---
+++ Jarvis/Models/FRD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Jarvis.Models
+{
+    public class FRD
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Name { get; set; }
+
+        [Required]
+        public int EmployeeId { get; set; }
+    }
+}
