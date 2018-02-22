9615652be78336bfd9a9f6c80ddfc1b5be074be5
James Kamana <jaykamana@gmail.com>
2018-02-22 19:54:50 +0300

    Update App name

---
+++ dbo.AspNetRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'3cdc4f2a-692d-423d-80e5-66413f6d4e93', N'CanManageFRD')
---
+++ dbo.AspNetUserRoles.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'9c71c0a5-1a21-43db-802c-857e44caa398', N'3cdc4f2a-692d-423d-80e5-66413f6d4e93')
